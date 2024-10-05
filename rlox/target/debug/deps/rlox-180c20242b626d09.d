ef9a4e89519361194a5a63aac2b14f6422fc8d90
thinhlq170 <thinh.lq170@gmail.com>
2024-10-05 21:24:18 +0700

    init cargo

---
+++ rlox/target/debug/deps/rlox-180c20242b626d09.d
@@ -0,0 +1,5 @@
+/home/thinhlq/Learning/Clox/my_rlox/rlox/target/debug/deps/librlox-180c20242b626d09.rmeta: src/main.rs
+
+/home/thinhlq/Learning/Clox/my_rlox/rlox/target/debug/deps/rlox-180c20242b626d09.d: src/main.rs
+
+src/main.rs:
---
+++ rlox/target/debug/deps/rlox-7b5840fff0b8e77c.d
@@ -0,0 +1,5 @@
+/home/thinhlq/Learning/Clox/my_rlox/rlox/target/debug/deps/librlox-7b5840fff0b8e77c.rmeta: src/main.rs
+
+/home/thinhlq/Learning/Clox/my_rlox/rlox/target/debug/deps/rlox-7b5840fff0b8e77c.d: src/main.rs
+
+src/main.rs:
